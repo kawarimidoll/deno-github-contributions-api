5f75dbe06be2def59ac658c996ad005078b52be1
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-01 14:56:19 +0900

    add functions to utils

--- utils.ts
+++ utils.ts
@@ -1,16 +1,21 @@
-const hexToRgbObj = (color = "000000") =>
-  Object.fromEntries(
-    (color.replace(/^#?(.*)$/, (_, hex) =>
-      (hex.length == 3) ? hex.replace(/./g, "$&$&") : hex)
-      .match(/../g) ?? []).map((c, i) => ["rgb".charAt(i), parseInt("0x" + c)]),
-  ) as { r: number; g: number; b: number };
+const confirmHex = (str: string) =>
+  str.match(/^#?[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/) ? str : "000";
 
-const hexToRgbNum = (color = "000000") =>
-  parseInt(
-    "0x" + color.replace(
-      /^#?(.*)$/,
-      (_, hex) => (hex.length == 3) ? hex.replace(/./g, "$&$&") : hex,
-    ),
+const convertToSixChars = (str: string) =>
+  confirmHex(str).replace(
+    /^#?(.*)$/,
+    (_, hex) => (hex.length == 3) ? hex.replace(/./g, "$&$&") : hex,
   );
 
-export { hexToRgbNum, hexToRgbObj };
+const hexToRgbObj = (color = "000") =>
+  Object.fromEntries(
+    (convertToSixChars(color).match(/../g) ?? []).map((
+      c,
+      i,
+    ) => ["rgb".charAt(i), parseInt("0x" + c)]),
+  );
+
+const hexToRgbNum = (color = "000") =>
+  parseInt("0x" + convertToSixChars(color));
+
+export { confirmHex, convertToSixChars, hexToRgbNum, hexToRgbObj };
--- utils_test.ts
+++ utils_test.ts
@@ -1,5 +1,30 @@
 import { assertEquals } from "./deps.ts";
-import { hexToRgbNum, hexToRgbObj } from "./utils.ts";
+import {
+  confirmHex,
+  convertToSixChars,
+  hexToRgbNum,
+  hexToRgbObj,
+} from "./utils.ts";
+
+Deno.test("confirmHex", () => {
+  assertEquals(confirmHex("#123456"), "#123456");
+  assertEquals(confirmHex("123456"), "123456");
+  assertEquals(confirmHex("#123"), "#123");
+  assertEquals(confirmHex("123"), "123");
+  assertEquals(confirmHex("#12345"), "000");
+  assertEquals(confirmHex("12345"), "000");
+  assertEquals(confirmHex("#12"), "000");
+  assertEquals(confirmHex("12"), "000");
+  assertEquals(confirmHex("hex"), "000");
+});
+
+Deno.test("convertToSixChars", () => {
+  assertEquals(convertToSixChars("#123456"), "123456");
+  assertEquals(convertToSixChars("123456"), "123456");
+  assertEquals(convertToSixChars("#123"), "112233");
+  assertEquals(convertToSixChars("123"), "112233");
+  assertEquals(convertToSixChars("12"), "000000");
+});
 
 Deno.test("hexToRgbObj", () => {
   assertEquals(hexToRgbObj("#123456"), { r: 18, g: 52, b: 86 });
