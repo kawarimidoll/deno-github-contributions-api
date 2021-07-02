83c824508276e796756aca6b29de7c2adce71a07
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-02 09:30:15 +0900

    fix hexToRgb

--- utils.ts
+++ utils.ts
@@ -1,5 +1,7 @@
+const defaultColor = "eee";
+
 const confirmHex = (str: string) =>
-  /^#?[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/.test(str) ? str : "000";
+  /^#?([0-9a-f]{3}){1,2}$/i.test(str) ? str : defaultColor;
 
 const convertToSixChars = (str: string) =>
   confirmHex(str).replace(
@@ -7,7 +9,7 @@ const convertToSixChars = (str: string) =>
     (_, hex) => (hex.length == 3) ? hex.replace(/./g, "$&$&") : hex,
   );
 
-const hexToRgbObj = (color = "000") =>
+const hexToRgbObj = (color = defaultColor) =>
   Object.fromEntries(
     (convertToSixChars(color).match(/../g) ?? []).map((
       c,
@@ -15,7 +17,7 @@ const hexToRgbObj = (color = "000") =>
     ) => ["rgb".charAt(i), parseInt("0x" + c)]),
   );
 
-const hexToRgbNum = (color = "000") =>
+const hexToRgbNum = (color = defaultColor) =>
   parseInt("0x" + convertToSixChars(color));
 
 export { confirmHex, convertToSixChars, hexToRgbNum, hexToRgbObj };
--- utils_test.ts
+++ utils_test.ts
@@ -9,33 +9,33 @@ import {
 Deno.test("confirmHex", () => {
   assertEquals(confirmHex("#123456"), "#123456");
   assertEquals(confirmHex("123456"), "123456");
-  assertEquals(confirmHex("#123"), "#123");
-  assertEquals(confirmHex("123"), "123");
-  assertEquals(confirmHex("#12345"), "000");
-  assertEquals(confirmHex("12345"), "000");
-  assertEquals(confirmHex("#12"), "000");
-  assertEquals(confirmHex("12"), "000");
-  assertEquals(confirmHex("hex"), "000");
+  assertEquals(confirmHex("#12A"), "#12A");
+  assertEquals(confirmHex("12A"), "12A");
+  assertEquals(confirmHex("#12345"), "eee");
+  assertEquals(confirmHex("12345"), "eee");
+  assertEquals(confirmHex("#12"), "eee");
+  assertEquals(confirmHex("12"), "eee");
+  assertEquals(confirmHex("hex"), "eee");
 });
 
 Deno.test("convertToSixChars", () => {
   assertEquals(convertToSixChars("#123456"), "123456");
   assertEquals(convertToSixChars("123456"), "123456");
-  assertEquals(convertToSixChars("#123"), "112233");
-  assertEquals(convertToSixChars("123"), "112233");
-  assertEquals(convertToSixChars("12"), "000000");
+  assertEquals(convertToSixChars("#12A"), "1122AA");
+  assertEquals(convertToSixChars("12A"), "1122AA");
+  assertEquals(convertToSixChars("12"), "eeeeee");
 });
 
 Deno.test("hexToRgbObj", () => {
   assertEquals(hexToRgbObj("#123456"), { r: 18, g: 52, b: 86 });
   assertEquals(hexToRgbObj("123456"), { r: 18, g: 52, b: 86 });
-  assertEquals(hexToRgbObj("#123"), { r: 17, g: 34, b: 51 });
-  assertEquals(hexToRgbObj("123"), { r: 17, g: 34, b: 51 });
+  assertEquals(hexToRgbObj("#12A"), { r: 17, g: 34, b: 170 });
+  assertEquals(hexToRgbObj("12A"), { r: 17, g: 34, b: 170 });
 });
 
 Deno.test("hexToRgbNum", () => {
   assertEquals(hexToRgbNum("#123456"), 0x123456);
   assertEquals(hexToRgbNum("123456"), 0x123456);
-  assertEquals(hexToRgbNum("#123"), 0x112233);
-  assertEquals(hexToRgbNum("123"), 0x112233);
+  assertEquals(hexToRgbNum("#12A"), 0x1122aa);
+  assertEquals(hexToRgbNum("12A"), 0x1122aa);
 });
