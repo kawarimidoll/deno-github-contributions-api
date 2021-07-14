f6ece220d1b52af24515eee67a23a198682c18bd
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-14 13:27:12 +0900

    fix(deps): update deno.land/std

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
 import ky from "https://cdn.skypack.dev/ky?dts";
 import stringWidth from "https://cdn.skypack.dev/string-width";
-import { bgRgb24, rgb24 } from "https://deno.land/std@0.99.0/fmt/colors.ts";
+import { bgRgb24, rgb24 } from "https://deno.land/std@0.101.0/fmt/colors.ts";
 import {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.99.0/testing/asserts.ts";
+} from "https://deno.land/std@0.101.0/testing/asserts.ts";
 
 import { Env } from "https://deno.land/x/env@v2.2.0/env.js";
 const env = new Env();
